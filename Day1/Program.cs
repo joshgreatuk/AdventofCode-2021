673cf21ae2ffe4e9cb9af32365cfee36ae9578dd
Joshua Bissell <joahgreat12@gmail.com>
2021-12-02 16:36:18 +0000

    Day 1 + Day 2

---
+++ Day1/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace AdventofCode_2021
+{
+    class Program
+    {
+        static string fileInput = "SweepReport.txt";
+
+        static void Main(string[] args)
+        {
+            //Here is where base code goes for loading input files and calling functions
+            Console.WriteLine("AOC2021 - Day 1 - Sonar Sweep");
+
+            int[] input = File.ReadAllLines(fileInput).Select(int.Parse).ToArray();
+            if (input.Length <= 2) { Console.WriteLine("Data source too short"); return; }
+            Console.WriteLine("Part 1 solution is: " + Part1(input).ToString());
+            if (input.Length <= 6) { Console.WriteLine("Data source too short"); return; }
+            Console.WriteLine("Part 2 solution is: " + Part2(input).ToString());
+
+            Console.WriteLine("Finished!");
+        }
+
+        static int Part1(int[] input)
+        {
+            int increases = 0;
+            int previous = input[0];
+
+            for (int i = 1; i < input.Length; i++)
+            {
+                if (input[i] > previous)
+                {
+                    increases += 1;
+                }
+                previous = input[i];
+            }
+            return increases;
+        }
+
+        static int Part2(int[] input)
+        {
+            int increases = 0;
+            int[] measurement = {input[0], input[1], input[2]};
+            int previous = measurement.Sum();
+            int lastIndex = 0;
+            
+            for (int i = 3; i < input.Length; i++)
+            {
+                if (lastIndex == 3) 
+                { 
+                    lastIndex = 0; 
+                }
+
+                previous = measurement.Sum();
+                measurement[lastIndex] = input[i];
+                lastIndex += 1;
+
+                if (measurement.Sum() > previous)
+                {
+                    increases += 1;
+                }
+            }
+            return increases;
+        }
+    }
+}
---
+++ Day2/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+
+namespace Day2
+{
+    class Program
+    {
+        static string fileInput = "SubCode.txt";
+
+        static void Main(string[] args)
+        {
+            //Here is where base code goes for loading input files and calling functions
+            Console.WriteLine("AOC2021 - Day 2 - Dive!");
+
+            Stopwatch timer = new Stopwatch();
+            timer.Start();
+
+            string[] inputRaw = File.ReadAllLines(fileInput);
+            if (inputRaw.Length%2 != 0) { Console.WriteLine("Data source not even"); return; }
+            int arrayLen = inputRaw.Length;
+
+            string[,] input = new string[arrayLen, 2];
+            for (int i = 0; i < arrayLen; i++)
+            {
+                string[] arrayContent = inputRaw[i].Split(' ');
+                for (int j = 0; j < 2; j++)
+                {
+                    input[i,j] = arrayContent[j];
+                }
+            }
+            
+            Console.WriteLine("File parsed in '" + timer.ElapsedMilliseconds.ToString() + "' milliseconds");
+            timer.Restart();
+
+            if (input.Length <= 1) { Console.WriteLine("Data source too short"); return; }
+            int solution = Part1(input);
+            Console.WriteLine("Got part 1 solution as '" + solution.ToString() + "' in '" + timer.ElapsedMilliseconds.ToString() + "' milliseconds");
+            timer.Restart();
+
+            if (input.Length <= 1) { Console.WriteLine("Data source too short"); return; }
+            solution = Part2(input);
+            Console.WriteLine("Got part 2 solution as '" + solution.ToString() + "' in '" + timer.ElapsedMilliseconds.ToString() + "' milliseconds");
+            timer.Stop();
+
+            Console.WriteLine("Finished!");
+        }
+
+        static int Part1 (string[,] input)
+        {
+            int horizontalPos = 0;
+            int depth = 0;
+
+            for (int i = 0; i < input.Length/2; i++)
+            {
+                switch (input[i,0])
+                {
+                    case "forward":
+                        horizontalPos += Int32.Parse(input[i,1]);
+                        break;
+                    case "up":
+                        depth -= Int32.Parse(input[i,1]);
+                        break;
+                    case "down":
+                        depth += Int32.Parse(input[i,1]);
+                        break;
+                }
+            }
+
+            return horizontalPos * depth;
+        }
+
+        static int Part2 (string[,] input)
+        {
+            int horizontalPos = 0;
+            int depth = 0;
+            int aim = 0;
+
+            for (int i = 0; i < input.Length/2; i++)
+            {
+                switch (input[i, 0])
+                {
+                    case "forward":
+                        horizontalPos += Int32.Parse(input[i, 1]);
+                        depth += aim * Int32.Parse(input[i, 1]);
+                        break;
+                    case "up":
+                        aim -= Int32.Parse(input[i, 1]);
+                        break;
+                    case "down":
+                        aim += Int32.Parse(input[i, 1]);
+                        break;
+                }   
+            }
+
+            return horizontalPos * depth;
+        }
+    }
+}
