06cac9e235bde87b341d8b67122a3abb96a9ac9f
joshgreatuk <joahgreat12@gmail.com>
2021-12-03 12:07:53 +0000

    Day 3 Progress
    Day 3 part 1 complete
    part 2 started, needs optimisation + finishing

--- Day3/Program.cs
+++ Day3/Program.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Linq;
 using System.Diagnostics;
+using System.Text;
 
 namespace AdventofCode_2021
 {
@@ -34,21 +35,27 @@ namespace AdventofCode_2021
             Console.WriteLine("Finished!");
         }
 
-        public int BinaryToDenary(string input)
+        static public int BinaryToDenary(string input)
         {
             int factor = 1;
             int answer = 0;
 
+            int[] inputArray = new int[input.Length];
             for (int i=0; i < input.Length; i++)
             {
-                answer += Int32.Parse(input[-i]) * factor;
+                inputArray[i] = Int32.Parse(input[i].ToString());
+            }
+
+            for (int i=1; i < input.Length+1; i++)
+            {
+                answer += inputArray[input.Length-i] * factor;
                 factor *= 2;
             }
 
             return answer;
         }
 
-        public int Part1(string[] input)
+        static public int Part1(string[] input)
         {
             int[] bitCount = new int[input[0].Length];
 
@@ -56,46 +63,91 @@ namespace AdventofCode_2021
             {
                 for (int j=0; j < input.Count(); j++)
                 {
-                    if (input[j][i] == 1)
+                    if (input[j][i] == '1')
                     {
                         bitCount[i] += 1;
                     }
                 }
             }
 
-            string mostCommon = "00000";
+            string mostCommon = "";
 
             for (int i=0; i < bitCount.Count(); i++)
             {
                 if (bitCount[i] >= input.Count()/2)
                 {
-                    mostCommon[i] = "1";
+                    mostCommon += "1";
                 }
                 else
                 {
-                    mostCommon[i] = "0";
+                    mostCommon += "0";
                 }
             }
 
-            string leastCommon = "00000";
+            string leastCommon = "";
 
             for (int i=0; i < mostCommon.Length; i++)
             {
-                if (mostCommon[i] == "1")
+                if (mostCommon[i] == '1')
                 {
-                    leastCommon[i] = "0";
+                    leastCommon += "0";
                 }
                 else
                 {
-                    leastCommon[i] = "1";
+                    leastCommon += "1";
                 }
             }
 
             return BinaryToDenary(mostCommon) * BinaryToDenary(leastCommon);
         }
 
-        public int Part2(string[] input)
+        //There must be a more efficient way to do this
+        static public int Part2(string[] input)
         {
+            string[] oxygenSortArray = input;
+            int currentBit = 0;
+            int chosenBit = 0;
+            while (oxygenSortArray.Count() > 2)
+            {
+                int bitCount = 0;
+                for (int i=0; i < oxygenSortArray.Count(); i++)
+                {
+                    if (oxygenSortArray[i][currentBit] == '1')
+                    {
+                        bitCount += 1;
+                    }
+                }
+                if (bitCount >= oxygenSortArray.Count()/2)
+                {
+                    chosenBit = 1;
+                }
+                else
+                {
+                    chosenBit = 0;
+                }
+
+                string[] tempSortArray;
+                if (chosenBit == 1)
+                {
+                    tempSortArray = new string[bitCount];
+                }
+                else
+                {
+                    tempSortArray = new string[oxygenSortArray.Count()-bitCount];
+                }
+                int currentIndex = 0;
+                for (int i=0; i < oxygenSortArray.Count(); i++)
+                {
+                    if (oxygenSortArray[i][currentBit].ToString() == chosenBit.ToString())
+                    {
+                        tempSortArray[currentIndex] = oxygenSortArray[i];
+                        currentIndex += 1;
+                    }
+                }
+                oxygenSortArray = tempSortArray;
+                currentBit += 1;
+            }
+            
             return 0;
         }
     }
